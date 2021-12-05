2e6615ade3368e4c093235832f85192d0b0366f1
hnguyen412 <hienngominh.nguyen@dxc.com>
2021-12-05 19:04:01 +0700

    add security basic (without database)

--- atbm/src/main/java/com/projectcuoiky/atbm/controllers/MainController.java
+++ atbm/src/main/java/com/projectcuoiky/atbm/controllers/MainController.java
@@ -1,39 +1,47 @@
 package com.projectcuoiky.atbm.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+//@PreAuthorize("isAuthenticated()")
 public class MainController {
 
-
-    @RequestMapping(value = {"", "/", "home"})
+    @PreAuthorize("hasRole('ROLE_User')")
+    @RequestMapping(value = { "/", "home"})
     public String Home() {
         return "views/Home";
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_User','ROLE_Admin')")
     @RequestMapping("/cart")
     public String CartItemList() {
         return "views/Cart";
     }
 
+    @PreAuthorize("permitAll()")
     @RequestMapping("/signup")
     public String signUp() {
         return "views/Signup";
     }
 
+    @PreAuthorize("permitAll()")
     @RequestMapping("/signin")
     public String signIn() {
         return "views/Signin";
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_User','ROLE_Admin')")
     @RequestMapping("/productDetail")
     public String productDetail() {
         return "views/product-detail";
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_User','ROLE_Admin')")
     @RequestMapping("/shop")
     public String shopListFull() {
         return "views/Shop";
     }
+
 }
---
+++ atbm/src/main/java/com/projectcuoiky/atbm/security/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.projectcuoiky.atbm.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+////                .antMatchers("/home").hasRole("User")
+////                .antMatchers("/signin", "/signup").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/signin").permitAll()
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/home")
+                .loginProcessingUrl("/signin")
+                .failureUrl("/signin?error==true")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .deleteCookies("JSESSIONID")
+                .and()
+                .exceptionHandling().accessDeniedPage("/accessDenied");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password(passwordEncoder().encode("user1Pass")).roles("User")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("adminPass")).roles("Admin");
+    }
+}
