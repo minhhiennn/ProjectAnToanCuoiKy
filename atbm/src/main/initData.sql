57518bd36d62dce75174eabfaa3df3fc682669b6
tranmanhtuong <noobtohero1345@gmail.com>
2021-12-16 22:36:11 +0700

    ok

--- atbm/src/main/initData.sql
+++ atbm/src/main/initData.sql
@@ -2,8 +2,20 @@ use ProjectCuoiKy;
 go
 
 -- insert product
-INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product1',20.0);
-INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product2',20.0);
+INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product1',210.0);
+INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product2',120.0);
+INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product11',420.0);
+INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product22',260.0);
+INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product13',270.0);
+INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product25',270.0);
+INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product11',270.0);
+INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product21',220.0);
+INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product12',220.0);
+INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product25',210.0);
+INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product16',250.0);
+INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product27',260.0);
+INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product12',260.0);
+INSERT INTO product (product_discount_price,in_stock,product_name,product_price) VALUES (24,200,'product29',260.0);
 
 INSERT INTO users (email,password) values ('emlama','ssssssssss');
 
--- atbm/src/main/java/com/projectcuoiky/atbm/controllers/MainController.java
+++ atbm/src/main/java/com/projectcuoiky/atbm/controllers/MainController.java
@@ -1,25 +1,32 @@
 package com.projectcuoiky.atbm.controllers;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
-
-import com.projectcuoiky.atbm.entities.Product;
-import com.projectcuoiky.atbm.entities.User;
-import com.projectcuoiky.atbm.service.ProductService;
-import com.projectcuoiky.atbm.service.UserRegisterService;
-import org.springframework.data.repository.query.Param;
-import org.springframework.ui.Model;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 
+import com.projectcuoiky.atbm.dtos.ProductDto;
+import com.projectcuoiky.atbm.entities.Product;
+import com.projectcuoiky.atbm.entities.User;
+import com.projectcuoiky.atbm.service.ProductService;
+import com.projectcuoiky.atbm.service.UserRegisterService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
-//@PreAuthorize("isAuthenticated()")
+// @PreAuthorize("isAuthenticated()")
 public class MainController {
 
     @Autowired
@@ -29,7 +36,7 @@ public class MainController {
     private UserRegisterService userRegisterService;
 
     @PreAuthorize("hasRole('ROLE_USER')")
-    @RequestMapping(value = {"/", "home"})
+    @RequestMapping(value = { "/", "home" })
     public String Home() {
         return "views/Home";
     }
@@ -49,9 +56,11 @@ public class MainController {
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String postSignUp(@ModelAttribute("user") User user, Model model, HttpServletRequest request) throws UnsupportedEncodingException, MessagingException {
+    @ResponseBody
+    public String postSignUp(@ModelAttribute("user") User user, Model model, HttpServletRequest request)
+            throws UnsupportedEncodingException, MessagingException {
         userRegisterService.register(user, getSiteURL(request));
-        model.addAttribute("message","wasSend");
+        // model.addAttribute("message","wasSend");
         return "success";
     }
 
@@ -73,22 +82,48 @@ public class MainController {
         return "views/product-detail";
     }
 
-    @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
+    // @PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
     @RequestMapping("/shop")
-    public String shopListFull(Model model) {
-        List<Product> list = productService.getAllProduct();
-        model.addAttribute("listProducts", list);//tuoi loz
-        System.out.println(list);
+    // public String shopListFull(Model model) {
+    // List<Product> list = productService.getAllProduct();
+    // model.addAttribute("listProducts", list);//tuoi loz
+    // System.out.println(list);
+    // return "views/Shop";
+    // }
+    public String shopPage(Model model, @RequestParam(required = false, name = "page") String Spage,
+            @RequestParam(required = false, name = "size") String Ssize,
+            @RequestParam(required = false, name = "sortBy") String sortBy,
+            @RequestParam(required = false, name = "desc") String Sdesc) {
+        try {
+            int page = (Spage == null ? 1 : Integer.parseInt(Spage));
+            int size = (Ssize == null ? 12 : Integer.parseInt(Ssize));
+            boolean desc = ((Sdesc != null && Sdesc.contains("true")) ? true : false);
+            List<Product> listProducts = productService.getAllListProductByPage((page - 1), size, sortBy, desc);
+            List<ProductDto> dtos = new ArrayList<>();
+
+            for (Product product : listProducts) {
+                dtos.add(new ProductDto(product.getId(), product.getName(), product.getPrice(),
+                        product.getDiscountPrice(), product.getInStock()));
+            }
+            model.addAttribute("listProducts", dtos);
+            model.addAttribute("pageMax", productService.getMaxPage(size));
+            model.addAttribute("currentPage", page);
+            model.addAttribute("shows", size);
+            model.addAttribute("sortBy", (sortBy != null ? sortBy : "id"));
+            model.addAttribute("desc", String.valueOf(desc));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return "views/Shop";
     }
 
     @PreAuthorize("permitAll()")
     @GetMapping("/verify")
-    public String verifyUser(@Param("code") String code,Model model) {
+    public String verifyUser(@Param("code") String code, Model model) {
         if (userRegisterService.verify(code)) {
             return "redirect:/signin";
         } else {
-            model.addAttribute("message","verifyError");
+            model.addAttribute("message", "verifyError");
             return "views/Signup";
         }
     }
